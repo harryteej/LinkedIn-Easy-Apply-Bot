5c026865e56abfa30f97223d98905e2e2b891dbf
harryteej <44134982+harryteej@users.noreply.github.com>
2023-10-10 13:49:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ from validate_email import validate_email
 def init_browser():
     browser_options = Options()
     options = webdriver.ChromeOptions()
+    '''new'''
+    #options.headless = True
     options = ['--disable-blink-features', '--no-sandbox', '--start-maximized', '--disable-extensions',
                '--ignore-certificate-errors', '--disable-blink-features=AutomationControlled']
 
     for option in options:
         browser_options.add_argument(option)
+        #print (ChromeDriverManager())
+        
 
-    #driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=browser_options)
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=browser_options)
+    #driver = webdriver.Chrome(ChromeDriverManager().install(), options= browser_options)
     
     driver.set_window_position(0, 0)
     driver.maximize_window()
